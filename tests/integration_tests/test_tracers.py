eab00c579c180bce8c205c02a67bdb0f205ecba0
mmsqe <tqd0800210105@gmail.com>
2024-01-29 18:43:01 +0800

    Problem: dependency is outdated (#403)
    Co-authored-by: yihuang <huang@crypto.com>

--- tests/integration_tests/test_tracers.py
+++ tests/integration_tests/test_tracers.py
@@ -24,31 +24,38 @@ from .utils import (
 )
 
 
-def test_trace_transactions_tracers(ethermint_rpc_ws):
-    w3: Web3 = ethermint_rpc_ws.w3
-    eth_rpc = w3.provider
-    gas_price = w3.eth.gas_price
-
-    tx = {"to": ADDRS["community"], "value": 100, "gasPrice": gas_price}
-    tx_hash = send_transaction(w3, tx)["transactionHash"].hex()
+def test_trace_transactions_tracers(ethermint, geth):
     method = "debug_traceTransaction"
     tracer = {"tracer": "callTracer"}
-    tx_res = eth_rpc.make_request(method, [tx_hash])
-    assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
-    tx_res = eth_rpc.make_request(method, [tx_hash, tracer])
-    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
-    tx_res = eth_rpc.make_request(
-        method,
-        [tx_hash, tracer | {"tracerConfig": {"onlyTopCall": True}}],
-    )
-    assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
-    _, tx = deploy_contract(w3, CONTRACTS["TestERC20A"])
-    tx_hash = tx["transactionHash"].hex()
+    price = hex(88500000000)
+    acc = derive_new_account(7)
 
-    w3_wait_for_new_blocks(w3, 1)
-    tx_res = eth_rpc.make_request(method, [tx_hash, tracer])
-    tx_res["result"]["to"] = EXPECTED_CONTRACT_CREATE_TRACER["to"]
-    assert tx_res["result"] == EXPECTED_CONTRACT_CREATE_TRACER, ""
+    def process(w3):
+        fund_acc(w3, acc)
+        call = w3.provider.make_request
+        tx = {"to": ADDRS["community"], "value": 100, "gasPrice": price}
+        tx_hash = send_transaction(w3, tx)["transactionHash"].hex()
+        tx_res = call(method, [tx_hash])
+        assert tx_res["result"] == EXPECTED_STRUCT_TRACER, ""
+        tx_res = call(method, [tx_hash, tracer])
+        assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+        tx_res = call(
+            method,
+            [tx_hash, tracer | {"tracerConfig": {"onlyTopCall": True}}],
+        )
+        assert tx_res["result"] == EXPECTED_CALLTRACERS, ""
+        _, tx = deploy_contract(w3, CONTRACTS["TestERC20A"], key=acc.key)
+        tx_hash = tx["transactionHash"].hex()
+        w3_wait_for_new_blocks(w3, 1)
+        tx_res = call(method, [tx_hash, tracer])
+        return json.dumps(tx_res["result"], sort_keys=True)
+
+    providers = [ethermint.w3, geth.w3]
+    with ThreadPoolExecutor(len(providers)) as exec:
+        tasks = [exec.submit(process, w3) for w3 in providers]
+        res = [future.result() for future in as_completed(tasks)]
+        assert len(res) == len(providers)
+        assert (res[0] == res[-1] == EXPECTED_CONTRACT_CREATE_TRACER), res
 
 
 def fund_acc(w3, acc):
