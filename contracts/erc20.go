3b4caf9f30928ab4dddceb0b0f36cc88f114a2e5
Jetso Analin <jetyjetso@gmail.com>
2024-07-01 02:37:07 +0530

    Push0 Opcode: addional helper codes added

--- contracts/erc20.go
+++ contracts/erc20.go
@@ -1,51 +1,30 @@
-// Copyright 2022 Evmos Foundation
-// This file is part of the Ethermint Network packages.
-//
-// Ethermint is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// The Ethermint packages are distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-// GNU Lesser General Public License for more details.
-//
-// You should have received a copy of the GNU Lesser General Public License
-// along with the Ethermint packages. If not, see https://github.com/maalchain/maalchain_l1/blob/main/LICENSE
+// Copyright Tharsis Labs Ltd.(Evmos)
+// SPDX-License-Identifier:ENCL-1.0(https://github.com/evmos/evmos/blob/main/LICENSE)
 
 package contracts
 
 import (
-	_ "embed" // embed compiled smart contract
-	"encoding/json"
+	_ "embed"
 
-	"github.com/ethereum/go-ethereum/common"
+	contractutils "github.com/maalchain/maalchain_l1/contracts/utils"
 	evmtypes "github.com/maalchain/maalchain_l1/x/evm/types"
-
-	"github.com/maalchain/maalchain_l1/x/erc20/types"
 )
 
 var (
-	//go:embed compiled_contracts/ERC20MinterBurnerDecimals.json
-	ERC20MinterBurnerDecimalsJSON []byte //nolint: golint
+	// ERC20MinterBurnerDecimalsJSON are the compiled bytes of the ERC20MinterBurnerDecimalsContract
+	//
+	//go:embed solidity/ERC20MinterBurnerDecimals.json
+	ERC20MinterBurnerDecimalsJSON []byte
 
 	// ERC20MinterBurnerDecimalsContract is the compiled erc20 contract
 	ERC20MinterBurnerDecimalsContract evmtypes.CompiledContract
-
-	// ERC20MinterBurnerDecimalsAddress is the erc20 module address
-	ERC20MinterBurnerDecimalsAddress common.Address
 )
 
 func init() {
-	ERC20MinterBurnerDecimalsAddress = types.ModuleAddress
-
-	err := json.Unmarshal(ERC20MinterBurnerDecimalsJSON, &ERC20MinterBurnerDecimalsContract)
-	if err != nil {
+	var err error
+	if ERC20MinterBurnerDecimalsContract, err = contractutils.ConvertHardhatBytesToCompiledContract(
+		ERC20MinterBurnerDecimalsJSON,
+	); err != nil {
 		panic(err)
-	}
-
-	if len(ERC20MinterBurnerDecimalsContract.Bin) == 0 {
-		panic("load contract failed")
 	}
 }
